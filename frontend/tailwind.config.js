071d5e5feb384c9d1323eac0f80aa9f75bd82486
veebika18 <veebika1803@gmail.com>
2025-03-04 00:02:23 +0530

    UPDATE

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",         // If using a static index.html file
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all React components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
